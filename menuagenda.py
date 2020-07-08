3ca5f1dff3fd42854ca7a8edf76edc5de043548d
cislast <cislast@gmail.com>
2020-07-08 17:39:41 -0500

    Menu agregado

---
+++ menuagenda.py
@@ -0,0 +1,44 @@
+from agenda import Agenda
+from contacto import Contacto
+
+agenda = Agenda('nueva_agenda')
+contactos = agenda.obtenerContactos()
+
+def mostrar_menu():
+    print("Agenda")
+    print("Selecciona la acción que desees efectuar")
+    print("1. Agregar contactos")
+    print("2. Mostar contactos")
+    print("3. Consulta de contactos")
+    print("4. Actualizar contactos")
+    print("5. Eliminar contactos")
+    print("6. Salir")
+
+salida = "1"
+while(salida == "1"):
+    mostrar_menu()
+    entrada = input ("Escribe la opcion: ")
+    
+    if entrada == "1":
+        nuevo_contacto = Contacto(input("Ingresa el nombre: "))
+        nuevo_contacto.empresa = input("Ingresa empresa: ")
+        nuevo_contacto.correo = input("Ingresa correo: ")
+        nuevo_contacto.telefono = input("Ingresa telefono: ")
+        nuevo_contacto.nota = input("Ingresa nota: ")
+        contactos.append(nuevo_contacto)
+        agenda.agregarContactos(contactos)
+        agenda.guardar()
+        
+    elif entrada == "2":
+         print(agenda.mostrarContactos())
+    elif entrada == "3":
+         consulta = input ("Escriba el nombre a consultar ")
+         print(agenda.obtenerContacto(consulta))
+    elif entrada == "4":
+         print("")
+    elif entrada == "5":
+         print("")
+    elif entrada ==  "6":
+        exit()
+    else:
+        print("Opcion no valida")
